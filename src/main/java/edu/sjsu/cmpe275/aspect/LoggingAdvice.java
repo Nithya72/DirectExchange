2db17bc128f905c3500bd21c9ffa571b5b5c8dde
Nithya72 <nithya.anbalagan2@gmail.com>
2020-11-21 18:35:15 -0800

    Logging Aspect

--- src/main/java/edu/sjsu/cmpe275/aspect/LoggingAdvice.java
+++ src/main/java/edu/sjsu/cmpe275/aspect/LoggingAdvice.java
@@ -9,6 +9,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+/**
+ * AOP reference - https://www.youtube.com/watch?v=RVvKPP5HyaA&ab_channel=JavaTechie
+ */
+
 @Aspect
 @Component
 public class LoggingAdvice{
@@ -26,8 +30,10 @@ public class LoggingAdvice{
         ObjectMapper mapper = new ObjectMapper();
         String methodName = pjp.getSignature().getName();
         String className = pjp.getTarget().getClass().getName();
-
-        log.info("Method invoked: "+methodName+" of class: "+className);
-        return pjp.proceed();
+        Object[] args = pjp.getArgs();
+        log.info("Method invoked: "+methodName+" of Class: "+className+"-- Arguments: "+mapper.writeValueAsString(args));
+        Object object = pjp.proceed();
+        log.info("Method invoked: "+methodName+" of Class: "+className+"-- Repsonse: "+mapper.writeValueAsString(object));
+        return object;
     }
 }

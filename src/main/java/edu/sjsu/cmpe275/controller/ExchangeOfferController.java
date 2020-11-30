b35dac0a919ae4baae7a83dc4e2c6aae86e2ca71
BhavanaBS <bhavana.bs1566@gmail.com>
2020-11-30 00:25:01 -0800

    Added @PreAuthorize checks to offers to avoid other user accessing data
    Getting 403 when trying to access offers for different userId while using JWT of different user now.
    https://www.baeldung.com/spring-security-method-security

--- src/main/java/edu/sjsu/cmpe275/controller/ExchangeOfferController.java
+++ src/main/java/edu/sjsu/cmpe275/controller/ExchangeOfferController.java
@@ -5,6 +5,7 @@ import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,16 +21,18 @@ public class ExchangeOfferController {
     @Autowired
     private ExchangeOfferService exchangeOfferService;
 
+    @PreAuthorize("#userId == authentication.principal")
     @GetMapping(value = "/allOffers/{id}", produces = {"application/json"})
     public ResponseEntity<?> alloffers(@PathVariable(value = "id") String userId) {
         return exchangeOfferService.getOffersByOthers(Long.parseLong(userId));
     }
 
+    @PreAuthorize("#userId == authentication.principal")
     @GetMapping(value = "/myoffer/{id}", produces = {"application/json"})
     public ResponseEntity<?> myoffers(@PathVariable(value = "id") String userId) {
         return exchangeOfferService.getMyOffers(Long.parseLong(userId));
     }
-
+    @PreAuthorize("#userId == authentication.principal")
     @GetMapping(value = "/singlematch/{userId}/{remitAmount}/{srcCurrency}", produces = {"application/json"})
     public ResponseEntity<?> singleMatches(@PathVariable(value = "userId") String userId, @PathVariable(value = "remitAmount") String remitAmount,@PathVariable(value = "srcCurrency") String srcCurrency) {
 
@@ -39,7 +42,7 @@ public class ExchangeOfferController {
         return exchangeOfferService.getSingleMatches(userId1, remitAmount1, srcCurrency);
     }
 
-
+    @PreAuthorize("#userId == authentication.principal")
     @GetMapping(value = "/allmatches/{userId}/{remitAmount}/{srcCurrency}", produces = {"application/json"})
     public ResponseEntity<?> allmatches(@PathVariable(value = "userId") String userId, @PathVariable(value = "remitAmount") String remitAmount,@PathVariable(value = "srcCurrency") String srcCurrency) {
 

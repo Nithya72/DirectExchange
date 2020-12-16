07ae42bd9a9bf9739c149206e7be4c214b335577
BhavanaBS <bhavana.bs1566@gmail.com>
2020-12-15 22:50:49 -0800

    url changes frontend
    url changes frontend

--- frontend/src/helpers/serverDetails.js
+++ frontend/src/helpers/serverDetails.js
@@ -1,4 +1,3 @@
-// const backend = "http://ec2-34-220-75-120.us-west-2.compute.amazonaws.com:8080";
-const backend = "http://localhost:8080";
+const backend = "http://ec2-34-219-184-245.us-west-2.compute.amazonaws.com:8080";
 
 export default backend;

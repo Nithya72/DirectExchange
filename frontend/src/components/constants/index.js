79605c39e8ef911167667c3eab87f331a783e841
BhavanaBS <bhavana.bs1566@gmail.com>
2020-12-15 22:47:54 -0800

    Merge branch 'develop' of /Users/bhavana/Documents/GitHub/DirectExchange with conflicts.

--- frontend/src/components/constants/index.js
+++ frontend/src/components/constants/index.js
@@ -1,6 +1,8 @@
+// export const API_BASE_URL = 'http://ec2-34-220-75-120.us-west-2.compute.amazonaws.com:8080/directexchange';
 export const API_BASE_URL = 'http://localhost:8080/directexchange';
 export const ACCESS_TOKEN = 'accessToken';
 
+// export const OAUTH2_REDIRECT_URI = 'http://ec2-34-220-75-120.us-west-2.compute.amazonaws.com:3000/oauth2/redirect'
 export const OAUTH2_REDIRECT_URI = 'http://localhost:3000/oauth2/redirect'
 
 export const GOOGLE_AUTH_URL = API_BASE_URL + '/oauth2/authorization/google';
--- src/main/java/edu/sjsu/cmpe275/config/SecurityConfig.java
+++ src/main/java/edu/sjsu/cmpe275/config/SecurityConfig.java
@@ -62,7 +62,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedOrigin("http:localhost:3000");
         config.setAllowedMethods(Arrays.asList("POST", "OPTIONS", "GET", "DELETE", "PUT"));
         config.setAllowedHeaders(Arrays.asList("X-Requested-With", "Origin", "Content-Type", "Accept", "Authorization"));
         source.registerCorsConfiguration("/**", config);
